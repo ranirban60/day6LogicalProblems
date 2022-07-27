2b10b45feb587b5aa086bdd6aa7a7b724dfdc4d4
ranirban60@gmail.com <ranirban60@gmail.com>
2022-07-27 18:39:49 +0530

    Added Program Stop Watch

---
+++ src/main/java/com/bridgelabz/StopWatch.java
@@ -0,0 +1,51 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class StopWatch {
+
+        public long startTimer=0;
+        public long stopTimer=0;
+        public long elapsed;
+
+        //to start timer
+        public void start()
+        {
+            startTimer=System.currentTimeMillis();
+            System.out.println("Start Time is: "+startTimer);
+        }
+
+        // to stop timer
+        public void stop()
+        {
+            stopTimer=System.currentTimeMillis();
+            System.out.println("Stop Time is: "+stopTimer);
+        }
+
+        public long getElapsedTime()
+        {
+            elapsed=stopTimer-startTimer;
+            return elapsed;
+        }
+
+        public static void main(String[] args) throws Exception
+        {
+            StopWatch sw=new StopWatch();
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Press '1' to Start Time: ");
+            sc.nextInt();
+            sw.start();
+
+            System.out.println();
+            System.out.println("Press '2' to Stop Time: ");
+            sc.nextInt();
+            sw.stop();
+
+            long l=sw.getElapsedTime();
+            System.out.println();
+            System.out.println("Total Time Elapsed(in Milli Second) is:"+l);
+            System.out.println();
+            System.out.println("Converting Milli Second to seconds: "+(l/1000)+" sec");
+        }
+    }
+
